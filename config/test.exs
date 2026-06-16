2035b400041a60528afa28b917004c100689036b
Nicholas Moen <arcanemachine@gmail.com>
2026-06-15 20:49:46 -0600

    chore: add idiomatic Postgres defaults

--- config/test.exs
+++ config/test.exs
@@ -4,9 +4,10 @@ config :factory_man, ecto_repos: [FactoryManDemo.Repo]
 
 config :factory_man, FactoryManDemo.Repo,
   username: System.get_env("POSTGRES_USER", "postgres"),
-  password: System.get_env("POSTGRES_PASWORD", "your_postgres_password"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
   database: "factory_man_demo",
   hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  port: System.get_env("POSTGRES_PORT", "5432") |> String.to_integer(),
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
